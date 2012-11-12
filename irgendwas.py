a88bbcc60e1a7eae9f5c44c73edac5116435620f
motzaf <motzaf@gmail.com>
2012-11-12 09:10:27 +0100

    Generic Menu added

--- irgendwas.py
+++ irgendwas.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 # a rogue-like game by Motz
 
-for i in range(10):
-    print('there are {} peas'.format(i))
+#for i in range(10):
+#    print('there are {} peas'.format(i))
     
-    
+
+textmenu = '''     Generic Menu
+       (a) Menu A
+       (b) Menu B
+       (c) Menu C
+       (d) Menu D
+       (q) Quit
+'''
+print(textmenu)
+
+while True:
+    eingabe=input('\nEingabe: ')
+    if eingabe == '':
+        continue
+    if eingabe[0] in 'qQ':
+        break
+    elif eingabe[0] in 'aA':
+        print('\n Menu A')
+    elif eingabe[0] in 'bB':
+        print('\n Menu B')
+    elif eingabe[0] in 'cC':
+        print('\n Menu C')
+    elif eingabe[0] in 'dD':
+        print('\n Menu D')
+    else:
+        print('\n Falsche Eingabe')
+        
+        
 
