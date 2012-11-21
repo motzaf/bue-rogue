2700617371a1b14c74db665c9cfb31b8e07bc1f6
motzaf <motzaf@gmail.com>
2012-11-21 22:11:56 +0100

    tag 6

--- mensch.py
+++ mensch.py
@@ -1,45 +1,39 @@
 #!/usr/bin/python3
 import random
 
+##TODO Kommentar
+### file to list als eigenes modul
+### 
+
 class Mensch(object):
-        ''' der sstandard mitarbeiteter '''
+        ''' der standard mitarbeiteter '''
         nummer = 0 #Klassenattribut gehoert der gesamten Menschheit
-        ## Listen als Klassenattribute
-        # willpower
-        # speech
+        ## Listen als Klassenattribute (spart file-IO)        
+        farben=[]
+        vornamenf=[]
+        vornamenm=[]
+        nachnamen=[]
         
         def __init__(self):
-                ''' konstruktoirorr oder so'''
-                self.geschlecht=random.choice(['m','w'])
-                self.haarfarbe=random.choice(['blau','blond','schwarz','weiss','braun','rot'])
-                self.groesse=random.randint(150,210)
-                self.gewicht=random.randint(50,150)
-                Mensch.nummer+=1
-                self.nummer=Mensch.nummer
-                self.name=self.getName()
+            ''' konstruktoirorr oder so'''
+            self.geschlecht=random.choice(['m','w'])
+            self.haarfarbe=self.getColor()
+            self.groesse=random.randint(150,210)
+            self.gewicht=random.randint(50,150)
+            Mensch.nummer+=1
+            self.nummer=Mensch.nummer
+            self.name=self.getName()
                 
-        def schrumpf(self, factor=0.9):
-                ''' schrumpf mit fuctor'''
-                self.groesse *= factor
-                self.gewicht *= factor
-        def wachsen(self, factor=1.1):
-                ''' wachsen wir die ski '''
-                self.groesse *= factor
-                self.gewicht *= factor
         def getName(self):
-                if self.geschlecht=='m':
-                        vnf=open('vorname.m.txt')
-                        vn=vnf.readlines()
-                        vnf.close()
-                else:
-                        vnf=open('vorname.f.txt')
-                        vn=vnf.readlines()
-                        vnf.close()
-                nnf=open('nachname.txt')
-                nn=nnf.readlines()
-                nnf.close()
-                name=random.choice(vn)[:-1]+' '+random.choice(nn)[:-1]
-                return name
+            if self.geschlecht=='m':
+                vn=random.choice(Mensch.vornamenm)
+            else:
+                vn=random.choice(Mensch.vornamenf)
+            nn=random.choice(Mensch.nachnamen)
+            return vn[:-1]+' '+nn[:-1]
+                            
+        def getColor(self):
+            return random.choice(Mensch.farben)[:-1]
                 
 class Menschheit(object):
     menschen={}
@@ -47,18 +41,35 @@ class Menschheit(object):
 
 #menschheit=[]
 
-while True:
+def main():
 
-    menschling=Mensch()
-    #print(menschling.nummer)
-    Menschheit.menschen[menschling.nummer]=menschling
-    #menschheit.append(menschling)
+    ## reading files into lists
+    ## may use 'for' in future
+    
+    file=open('color.txt')
+    Mensch.farben=file.readlines()
+    file.close()
+    file=open('vorname.f.txt')
+    Mensch.vornamenf=file.readlines()
+    file.close()
+    file=open('vorname.m.txt')
+    Mensch.vornamenm=file.readlines()
+    file.close()
+    file=open('nachname.txt')
+    Mensch.nachnamen=file.readlines()
+    file.close()
+    
+    while True:
+        menschling=Mensch()
+        #print(menschling.nummer)
+        Menschheit.menschen[menschling.nummer]=menschling
+        #menschheit.append(menschling)
 
-    print(menschling.__dict__)
+        print(menschling.__dict__)
 
-    eingabe=input()
-    if eingabe=="q":
-        break
+        eingabe=input()
+        if eingabe=="q":
+            break
 
-
-
+if __name__=='__main__':
+    main()
